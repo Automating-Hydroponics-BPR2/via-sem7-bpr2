cb99ca5ca614925904c0186d929cfffed3497d55
NoHop3 <u_sux@abv.bg>
2023-09-27 11:59:15 +0200

    feat: adding user authentication in the form of AWS Cognito

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -6,6 +6,17 @@ export type AmplifyDependentResourcesAttributes = {
       "RootUrl": "string"
     }
   },
+  "auth": {
+    "viasem7bpr2client": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
   "function": {
     "devicesTriggere97ebce0": {
       "Arn": "string",
