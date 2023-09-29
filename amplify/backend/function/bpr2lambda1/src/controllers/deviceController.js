855deff2141dc2c99564a49e198b44f939de9b38
NoHop3 <u_sux@abv.bg>
2023-09-29 23:38:19 +0200

    feat(lambda backend): adding patch, delete & modifying existing get all methods
    Adding patch, delete & modifying existing get all methods. Change to getAllDevices naming. Exposing
    new delete & patch methods in deviceRouter

--- amplify/backend/function/bpr2lambda1/src/controllers/deviceController.js
+++ amplify/backend/function/bpr2lambda1/src/controllers/deviceController.js
@@ -28,11 +28,40 @@ export const createDevice = async (req, res, next) => {
   }
 };
 
+export const updateDeviceById = async (req, res, next) => {
+  try {
+    const { body: device } = req;
+    const { deviceId } = req.params;
+    const updatedDevice = await deviceServices.updateDeviceById(deviceId, device);
+    res.status(200).json(updatedDevice);
+  } catch (error) {
+    if (error instanceof Error && error.name === 'ValidationError') {
+      next(new BadRequestError('Invalid Request', error));
+    } else {
+      next(new ApiError(500, `Internal Server Error, ${error}`, 'src/controllers/deviceController.js - updateDeviceById'));
+    }
+  }
+}
+
+export const deleteDeviceById = async (req, res, next) => {
+  try {
+    const { deviceId } = req.params;
+    await deviceServices.deleteDeviceById(deviceId);
+    res.status(204).json();
+  } catch (error) {
+    if (error instanceof Error && error.name === 'ValidationError') {
+      next(new BadRequestError('Invalid Request', error));
+    } else {
+      next(new ApiError(500, `Internal Server Error, ${error}`, 'src/controllers/deviceController.js - deleteDeviceById'));
+    }
+  }
+}
+
 export const getDeviceById = async (req, res, next) => {
   try {
     const { deviceId } = req.params;
     console.log('getDeviceById', deviceId);
-    const device = await deviceServices.findById(deviceId);
+    const device = await deviceServices.getDeviceById(deviceId);
     res.status(200).json(device);
   } catch (error) {
     if (error instanceof Error && error.name === 'ValidationError') {
--- amplify/backend/function/bpr2lambda1/src/services/deviceService.js
+++ amplify/backend/function/bpr2lambda1/src/services/deviceService.js
@@ -1,5 +1,12 @@
 import { v4 as uuidv4 } from 'uuid';
-import { DynamoDBClient, ListTablesCommand, GetItemCommand, PutItemCommand } from '@aws-sdk/client-dynamodb';
+import {
+  DynamoDBClient,
+  GetItemCommand,
+  PutItemCommand,
+  DeleteItemCommand,
+  UpdateItemCommand,
+  ScanCommand,
+} from '@aws-sdk/client-dynamodb';
 import { NotFoundError } from '../helpers/apiError.js';
 const dynamoDb = new DynamoDBClient({ region: 'eu-central-1' });
 
@@ -27,24 +34,64 @@ const getDeviceById = async (deviceId) => {
   );
 
   if (!deviceToFetch) {
-    throw new NotFoundError(`Device with id ${deviceId} not found`, "src/services/deviceService.js - getDeviceById");
+    throw new NotFoundError(`Device with id ${deviceId} not found`, 'src/services/deviceService.js - getDeviceById');
   }
 
   return deviceToFetch;
 };
 
-const getAllDevices = async () => {
-  const devices = await dynamoDb.send(
-    new ListTablesCommand({
+const deleteDeviceById = async (deviceId) => {
+  const deviceToDelete = await dynamoDb.send(
+    new GetItemCommand({ TableName: 'devices-dev', Key: { id: { S: deviceId } } }),
+  );
+
+  if (!deviceToDelete) {
+    throw new NotFoundError(`Device with id ${deviceId} not found`, 'src/services/deviceService.js - deleteDeviceById');
+  }
+
+  await dynamoDb.send(new DeleteItemCommand({ TableName: 'devices-dev', Key: { id: { S: deviceId } } }));
+};
+
+const updateDeviceById = async (deviceId, device) => {
+  const deviceToUpdate = await dynamoDb.send(
+    new GetItemCommand({ TableName: 'devices-dev', Key: { id: { S: deviceId } } }),
+  );
+
+  if (!deviceToUpdate) {
+    throw new NotFoundError(`Device with id ${deviceId} not found`, 'src/services/deviceService.js - updateDeviceById');
+  }
+
+  await dynamoDb.send(
+    new UpdateItemCommand({
       TableName: 'devices-dev',
+      Key: { id: { S: deviceId } },
+      UpdateExpression: 'set name = :n, type = :t, reading = :r, dateTime = :d',
+      ExpressionAttributeValues: {
+        ':n': { S: device.name },
+        ':t': { S: device.type },
+        ':r': { S: device.reading },
+        ':d': { S: device.dateTime },
+      },
     }),
   );
 
-  return devices ?? [];
+  const deviceUpdated = await dynamoDb.send(
+    new GetItemCommand({ TableName: 'devices-dev', Key: { id: { S: deviceId } } }),
+  );
+
+  return deviceUpdated;
+};
+
+const getAllDevices = async (take, limit) => {
+  const devices = await dynamoDb.send(new ScanCommand({ TableName: 'devices-dev', Limit: limit, TotalSegments: take }));
+
+  return devices;
 };
 
 export const deviceServices = {
   createDevice,
-  findById: getDeviceById,
+  getDeviceById,
+  deleteDeviceById,
+  updateDeviceById,
   getAllDevices,
 };
