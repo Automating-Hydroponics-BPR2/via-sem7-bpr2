b68cd185a4316c63b9ab75f1f0eec09c903ffea2
NoHop3 <u_sux@abv.bg>
2023-09-29 10:19:20 +0200

    fix(lmabda function): now passing event instead of req, res

--- amplify/backend/function/bpr2lambda1/src/controllers/deviceController.js
+++ amplify/backend/function/bpr2lambda1/src/controllers/deviceController.js
@@ -1,19 +1,20 @@
 // deviceController.js
 import { deviceServices } from '../services/deviceService.js';
 
-export const getAllDevices = async (req, res, next) => {
+export const getAllDevices = async (event, req, res, next) => {
   const devices = await deviceServices.getAllDevices();
   res.status(200).json(devices).end();
 };
 
-export const createDevice = async (req, res, next) => {
+export const createDevice = async (event, req, res, next) => {
   const device = JSON.parse(req.body);
   const createdDevice = await deviceServices.createDevice(device);
   res.status(201).json(createdDevice).end();
 };
 
-export const findById = async (req, res, next) => {
-  const { deviceId } = req.pathParameters;
+export const findById = async (event, req, res, next) => {
+  console.log(event, req, res, next);
+  const { deviceId } = event.pathParameters;
   const device = await deviceServices.findById(deviceId);
   res.status(200).json(device).end();
 };
