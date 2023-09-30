c71d8a67af564623151d3cc054057083d7bf0784
NoHop3 <u_sux@abv.bg>
2023-09-30 16:43:33 +0200

    fix(backend > lambda > devicecontroller, deviceservice, apierror): better error handlin, service fix
    Better error handling, deviceService method fixes, adding try-catch blocks, adding DynamoDb error
    type in custom error handling middleware, adding some handling in deviceController as well.

--- amplify/backend/function/bpr2lambda1/src/services/deviceService.js
+++ amplify/backend/function/bpr2lambda1/src/services/deviceService.js
@@ -8,111 +8,158 @@ import {
   ScanCommand,
 } from '@aws-sdk/client-dynamodb';
 import { marshall, unmarshall } from '@aws-sdk/util-dynamodb';
-import { NotFoundError } from '../helpers/apiError.js';
+import { NotFoundError, DynamoDbError } from '../helpers/apiError.js';
 const dynamoDb = new DynamoDBClient({ region: 'eu-central-1' });
 
 const createDevice = async (device) => {
-  const { Attributes } = await dynamoDb.send(
-    new PutItemCommand({
-      TableName: process.env.DYNAMODB_TABLE_NAME,
-      Item: marshall({
-        id: uuidv4(),
-        name: device.name,
-        type: device.type,
-        reading: device.reading,
-        dateTime: device.dateTime,
-      }),
-      ReturnValues: 'ALL_OLD',
-    }),
-  );
+  try {
+    const deviceToCreate = {
+      id: uuidv4(),
+      name: device.name,
+      type: device.type,
+      reading: device.reading,
+      dateTime: device.dateTime,
+    };
 
-  return unmarshall(Attributes);
+    await dynamoDb.send(
+      new PutItemCommand({
+        TableName: process.env.DYNAMODB_TABLE_NAME,
+        Item: marshall(deviceToCreate),
+      }),
+    );
+
+    return deviceToCreate;
+  } catch (error) {
+    throw new DynamoDbError(error, 'src/services/deviceService.js - createDevice');
+  }
 };
 
 const getDeviceById = async (deviceId) => {
-  const { Item } = await dynamoDb.send(
-    new GetItemCommand({
-      TableName: process.env.DYNAMODB_TABLE_NAME,
-      Key: marshall({ id: deviceId }),
-    }),
-  );
-
-  if (!Item) {
-    throw new NotFoundError(
-      `Could not get device with id ${deviceId}, device not found`,
-      'src/services/deviceService.js - getDeviceById',
+  try {
+    const { Item } = await dynamoDb.send(
+      new GetItemCommand({
+        TableName: process.env.DYNAMODB_TABLE_NAME,
+        Key: marshall({ id: deviceId }),
+      }),
     );
-  }
 
-  return unmarshall(Item);
+    if (!Item) {
+      throw new NotFoundError(
+        `Could not get device with id ${deviceId}, device not found`,
+        'src/services/deviceService.js - getDeviceById',
+      );
+    }
+
+    return unmarshall(Item);
+  } catch (error) {
+    if (error instanceof NotFoundError) throw error;
+    else throw new DynamoDbError(error, 'src/services/deviceService.js - getDeviceById');
+  }
 };
 
 const deleteDeviceById = async (deviceId) => {
-  const { Item } = await dynamoDb.send(
-    new GetItemCommand({ TableName: process.env.DYNAMODB_TABLE_NAME, Key: marshall({ id: deviceId }) }),
-  );
-
-  if (!Item) {
-    throw new NotFoundError(
-      `Could not delete device with id ${deviceId}, device not found`,
-      'src/services/deviceService.js - deleteDeviceById',
+  try {
+    const { Item } = await dynamoDb.send(
+      new GetItemCommand({
+        TableName: process.env.DYNAMODB_TABLE_NAME,
+        Key: marshall({ id: deviceId }),
+      }),
     );
-  }
 
-  await dynamoDb.send(
-    new DeleteItemCommand({ TableName: process.env.DYNAMODB_TABLE_NAME, Key: marshall({ id: deviceId }) }),
-  );
+    if (!Item) {
+      throw new NotFoundError(
+        `Could not delete device with id ${deviceId}, device not found`,
+        'src/services/deviceService.js - deleteDeviceById',
+      );
+    }
+
+    await dynamoDb.send(
+      new DeleteItemCommand({
+        TableName: process.env.DYNAMODB_TABLE_NAME,
+        Key: marshall({ id: deviceId }),
+      }),
+    );
+  } catch (error) {
+    if (error instanceof NotFoundError) throw error;
+    else throw new DynamoDbError(error, 'src/services/deviceService.js - deleteDeviceById');
+  }
 };
 
 const updateDeviceById = async (deviceId, device) => {
-  const { Item } = await dynamoDb.send(
-    new GetItemCommand({ TableName: process.env.DYNAMODB_TABLE_NAME, Key: marshall({ id: deviceId }) }),
-  );
-
-  if (!Item) {
-    throw new NotFoundError(
-      `Could not update device with id ${deviceId}, device not found`,
-      'src/services/deviceService.js - updateDeviceById',
+  try {
+    const { Item } = await dynamoDb.send(
+      new GetItemCommand({ TableName: process.env.DYNAMODB_TABLE_NAME, Key: marshall({ id: deviceId }) }),
     );
+
+    if (!Item) {
+      throw new NotFoundError(
+        `Could not update device with id ${deviceId}, device not found`,
+        'src/services/deviceService.js - updateDeviceById',
+      );
+    }
+
+    const deviceToUpdateKeys = Object.keys(unmarshall(Item));
+
+    // Filter out keys that don't exist in the DynamoDB table
+    const validDeviceAttributes = Object.keys(device).filter((key) => deviceToUpdateKeys.includes(key));
+
+    const updateExpression = `SET ${validDeviceAttributes.map((key) => `#${key} = :${key}`).join(', ')}`;
+    const expressionAttributeNames = validDeviceAttributes.reduce((acc, key) => ({ ...acc, [`#${key}`]: key }), {});
+    const expressionAttributeValues = marshall(
+      validDeviceAttributes.reduce((acc, key) => ({ ...acc, [`:${key}`]: device[key] }), {}),
+    );
+
+    await dynamoDb.send(
+      new UpdateItemCommand({
+        TableName: process.env.DYNAMODB_TABLE_NAME,
+        Key: marshall({ id: deviceId }),
+        UpdateExpression: updateExpression,
+        ExpressionAttributeNames: expressionAttributeNames,
+        ExpressionAttributeValues: expressionAttributeValues,
+      }),
+    );
+
+    const { Item: updatedItem } = await dynamoDb.send(
+      new GetItemCommand({
+        TableName: process.env.DYNAMODB_TABLE_NAME,
+        Key: marshall({ id: deviceId }),
+      }),
+    );
+
+    return unmarshall(updatedItem);
+  } catch (error) {
+    if (error instanceof NotFoundError) throw error;
+    else throw new DynamoDbError(error, 'src/services/deviceService.js - updateDeviceById');
   }
-
-  const deviceToUpdateKeys = Object.keys(unmarshall(Item));
-
-  // Filter out keys that don't exist in the DynamoDB table
-  const validDeviceAttributes = Object.keys(device).filter((key) => deviceToUpdateKeys.includes(key));
-
-  const updateExpression = `SET ${validDeviceAttributes.map((key) => `#${key} = :${key}`).join(', ')}`;
-  const expressionAttributeNames = validDeviceAttributes.reduce((acc, key) => ({ ...acc, [`#${key}`]: key }), {});
-  const expressionAttributeValues = marshall(
-    validDeviceAttributes.reduce((acc, key) => ({ ...acc, [`:${key}`]: device[key] }), {}),
-  );
-
-  await dynamoDb.send(
-    new UpdateItemCommand({
-      TableName: process.env.DYNAMODB_TABLE_NAME,
-      Key: marshall({ id: deviceId }),
-      UpdateExpression: updateExpression,
-      ExpressionAttributeNames: expressionAttributeNames,
-      ExpressionAttributeValues: expressionAttributeValues,
-    }),
-  );
-
-  const { Item: updatedItem } = await dynamoDb.send(
-    new GetItemCommand({
-      TableName: process.env.DYNAMODB_TABLE_NAME,
-      Key: marshall({ id: deviceId }),
-    }),
-  );
-
-  return unmarshall(updatedItem);
 };
 
-const getAllDevices = async (limit, take) => {
-  const devices = await dynamoDb.send(
-    new ScanCommand({ TableName: process.env.DYNAMODB_TABLE_NAME, Limit: limit, TotalSegments: take }),
-  );
+const getAllDevices = async (start, limit) => {
+  try {
+    // start can be undefined, so we need to check if it exists
+    const data = await dynamoDb.send(
+      new ScanCommand({
+        TableName: process.env.DYNAMODB_TABLE_NAME,
+        Limit: limit,
+        ExclusiveStartKey: start && marshall({ id: start }),
+      }),
+    );
 
-  return devices.Items.map((device) => unmarshall(device));
+    if (!data)
+      throw new NotFoundError(
+        `Could not get all devices, no data found`,
+        'src/services/deviceService.js - getAllDevices',
+      );
+
+    const devicesToReturn = {
+      devices: data.Items.map((item) => unmarshall(item)),
+      count: data.Count,
+      lastEvaluatedKey: data.LastEvaluatedKey && unmarshall(data.LastEvaluatedKey),
+    };
+
+    return devicesToReturn;
+  } catch (error) {
+    throw new DynamoDbError(error, 'src/services/deviceService.js - getAllDevices');
+  }
 };
 
 export const deviceServices = {
