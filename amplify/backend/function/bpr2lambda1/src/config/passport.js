6293b1065f98a217cdc3367586e292817fea0d30
NoHop3 <u_sux@abv.bg>
2023-10-05 17:46:42 +0200

    fix(backend > lambda > passport.js): fixing wrong import

--- amplify/backend/function/bpr2lambda1/src/config/passport.js
+++ amplify/backend/function/bpr2lambda1/src/config/passport.js
@@ -1,5 +1,5 @@
 import { Strategy as JwtStrategy, ExtractJwt } from 'passport-jwt';
-import userServices from '../services/userServices.js';
+import userServices from '../services/userService.js';
 
 export const jwtStrategy = new JwtStrategy(
   {
