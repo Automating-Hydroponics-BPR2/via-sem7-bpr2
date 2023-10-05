a8a59b6518881a63ef3f1b960b3e26d77d023af4
NoHop3 <u_sux@abv.bg>
2023-10-05 13:41:14 +0200

    fix(backend > lambda > userservice): fixing checkIfUsernamExists method QueryCommand

--- amplify/backend/function/bpr2lambda1/src/services/userService.js
+++ amplify/backend/function/bpr2lambda1/src/services/userService.js
@@ -19,9 +19,9 @@ const checkIfUsernameExists = async (username, isReturnSpecified) => {
       new QueryCommand({
         TableName: process.env.DYNAMODB_TABLE_NAME_USERS,
         IndexName: 'username-index',
+        ExpressionAttributeNames: { '#username': ':username' },
+        ExpressionAttributeValues: marshall({ ':username': username }),
         KeyConditionExpression: '#username = :username',
-        ExpressionAttributeNames: { '#username': 'username' },
-        
       }),
     );
 
