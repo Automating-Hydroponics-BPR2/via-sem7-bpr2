4b0095022ff17ffdac1b8f7d9a38dbbd791c0baa
NoHop3 <u_sux@abv.bg>
2023-11-15 13:59:11 +0200

    fix: fixing failed main build on aws amplify

--- babel.config.js
+++ babel.config.js
@@ -1,19 +1,13 @@
 module.exports = {
   presets: [
-    [
+      '@babel/preset-typescript',
       '@babel/preset-env',
-      {
-        targets: {
-          node: 'current',
-        },
-      },
-    ],
-    [
-      '@babel/preset-react',
-      {
-        runtime: 'automatic',
-      },
-    ],
-    '@babel/preset-typescript',
+      ['@babel/preset-react', { runtime: 'automatic' }],
   ],
+  "plugins": [
+      ["@babel/plugin-proposal-class-properties", { "loose": true }],
+      ["@babel/plugin-proposal-private-methods", { "loose": true }],
+      ["@babel/plugin-proposal-private-property-in-object", { "loose": true }],
+      ["@babel/plugin-transform-private-property-in-object", { "loose": true }]
+  ]
 };
